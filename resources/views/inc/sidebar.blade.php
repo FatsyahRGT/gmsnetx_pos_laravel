5253b34407d0369a8c7f2db9fbd612ebdcd67d48
FatsyahRGT <rgtftsh@gmail.com>
2025-03-03 22:42:55 +0700

    Perapihan Dashboard 2

--- resources/views/inc/sidebar.blade.php
+++ resources/views/inc/sidebar.blade.php
@@ -9,18 +9,6 @@
     </div>    
     </div>
 
-    <!-- SidebarSearch Form -->
-    <div class="form-inline">
-      <div class="input-group" data-widget="sidebar-search">
-        <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-        <div class="input-group-append">
-          <button class="btn btn-sidebar">
-            <i class="fas fa-search fa-fw"></i>
-          </button>
-        </div>
-      </div>
-    </div>
-
     <!-- Sidebar Menu -->
     <nav class="mt-2">
       <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
@@ -62,9 +50,9 @@
             <li class="nav-item">
               <a href="{{ route('user.index')}}" class="nav-link">
                 <i class="far fa-circle nav-icon"></i>
-                <p>User</p>
+                <p>User Internal</p>
               </a>
-            </li>
+            </li> 
           </ul>
         </li>
         <li class="nav-item">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,7 +79,7 @@
                     </div>
                     <!-- /.card-body -->
                     <div class="card-footer">
-                        Footer
+                        
                     </div>
                     <!-- /.card-footer-->
                 </div>
@@ -113,8 +113,8 @@
     <!-- AdminLTE App -->
     <script src="{{ asset('adm/dist/js/adminlte.min.js') }}"></script>
     <!-- Bootstrap 5 CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script> --}}
 
     
     <!-- AdminLTE for demo purposes -->
